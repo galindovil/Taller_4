1e6ddf19b8f6d52378be0c54d195c6c0a0963507
Johan Galindo <jfgalindov@udistrital.edu.co>
2024-04-09 15:35:18 -0500

    version 3.0

--- programa.py
+++ programa.py
@@ -1,12 +1,4 @@
 import numpy as np
 
-miLista=[3,5,7,9]
-
-miArreglo=np.array(miLista)
-#atributos arreglos 
-print(miArreglo.ndim)
-print(miArreglo.shape)
-print(miArreglo.size)
-print(miArreglo.dtype)
-
-miArreglo2=np.array([3,6,7,90])
+miArreglo=np.linspace(0,100,10)
+print(miArreglo)
